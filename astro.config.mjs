c77abd17388b6137752cb29d33c742dd92ab181b
Kiwifu <kiwifucom@gmail.com>
2024-07-25 18:39:34 +0000

    Add Vercel adapter for SSR

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
+import vercel from '@astrojs/vercel/serverless';
 
 export default defineConfig({
   integrations: [tailwind(), react()],
   output: 'server',
+  adapter: vercel(),
   vite: {
     define: {
       'import.meta.env.PUBLIC_STRAPI_URL': JSON.stringify(process.env.PUBLIC_STRAPI_URL),
